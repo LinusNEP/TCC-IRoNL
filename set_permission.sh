fc735e162db48780e1c02447c9e56a33d26fe2c0
linuxnep <86525017+LinusNEP@users.noreply.github.com>
2024-02-07 15:08:38 +0100

    Update set_permission.sh

--- set_permission.sh
+++ set_permission.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Set permissions for all necessary scripts
+# This sets permissions for all the executable scripts
 chmod +x ~/catkin_ws/src/tcc-ironl/llm_nodes/llmNode_launcher.sh
 chmod +x ~/catkin_ws/src/tcc-ironl/vlm_nodes/vlmNode_launcher.sh
 
