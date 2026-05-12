ec15501ab6cd9d1f56fbfa86a773960075ea58e5
linuxnep <86525017+LinusNEP@users.noreply.github.com>
2026-05-12 12:04:46 +0200

    Delete set_permission.sh

--- set_permission.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This sets permissions for all the executable scripts
-chmod +x ~/catkin_ws/src/tcc-ironl/llm_nodes/llmNode_launcher.sh
-chmod +x ~/catkin_ws/src/tcc-ironl/vlm_nodes/vlmNode_launcher.sh
-
-echo "Permissions set for all scripts."
-
